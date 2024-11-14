d2f6e44aed1126b96e7808b645d3ae52ea3f5672
Brian Xu <mattx0121@gmail.com>
2024-11-13 18:17:22 -0800

    the loading gif not work

--- frontend/src/pages/Post/Post.page.jsx
+++ frontend/src/pages/Post/Post.page.jsx
@@ -13,18 +13,8 @@ export const PostPage = () => {
       <Suspense
         fallback={
           <Center style={{ height: "80vh" }}>
-            {/* Giphy embed code adjusted for React */}
-            <div style={{ width: "100%", height: 0, paddingBottom: "100%", position: "relative" }}>
-              <iframe
-                src="https://giphy.com/embed/l3nWhI38IWDofyDrW"
-                width="100%"
-                height="100%"
-                style={{ position: "absolute" }}
-                frameBorder="0"
-                allowFullScreen
-                title="loading-gif"
-              ></iframe>
-            </div>
+            {/* Reference the GIF using the public folder path */}
+            <img src="/loading.gif" alt="Loading..." style={{ width: "120px" }} />
           </Center>
         }
       >
